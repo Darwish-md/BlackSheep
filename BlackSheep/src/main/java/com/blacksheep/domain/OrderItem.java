68ed659fabd9e83de5a85f4843d6a2472992561e
Talha Yaseen Bhatti <79541998+talhayaseen57@users.noreply.github.com>
2021-03-30 12:33:22 +0200

    made entities and database

---
+++ BlackSheep/src/main/java/com/blacksheep/domain/OrderItem.java
@@ -0,0 +1,36 @@
+package com.blacksheep.domain;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+	
+	private OrderItemId pk;
+	private int unitPrice;
+	private int quantity;
+	
+	@EmbeddedId
+	public OrderItemId getPk() {
+		return pk;
+	}
+	public void setPk(OrderItemId pk) {
+		this.pk = pk;
+	}
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(int unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
---
+++ BlackSheep/src/main/java/com/blacksheep/domain/OrderItemId.java
@@ -0,0 +1,41 @@
+package com.blacksheep.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class OrderItemId implements Serializable{
+
+	private static final long serialVersionUID = -770856908208323295L;
+	private Product product;
+	private Order order;
+	
+	@ManyToOne
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	@ManyToOne
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		return super.equals(obj);
+	}
+	
+}
