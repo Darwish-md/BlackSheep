1007c9c2d80473bfceaef3c8fdff3f4b15298084
Talha Yaseen Zafar <tyaseen9876@gmail.com>
2021-03-30 23:35:27 +0200

    Models creation

---
+++ BlackSheep/src/main/java/com/blacksheep/BlackSheepApplication.java
@@ -0,0 +1,13 @@
+package com.blacksheep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackSheepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackSheepApplication.class, args);
+	}
+
+}
---
+++ BlackSheep/src/main/java/com/blacksheep/domain/Customer.java
@@ -0,0 +1,110 @@
+package com.blacksheep.domain;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String phone;
+	private String streetAddress;
+	private String postalCode;
+	private String city;
+	
+	private Set<Order> orders;
+	
+	public Customer() {}
+	
+	public Customer(String firstName, String lastName, String email, String password, String phone,
+			String streetAddress, String postalCode, String city) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.phone = phone;
+		this.streetAddress = streetAddress;
+		this.postalCode = postalCode;
+		this.city = city;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	
+	@OneToMany(mappedBy = "customer")
+	public Set<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Set<Order> orders) {
+		this.orders = orders;
+	}
+	
+	
+}
