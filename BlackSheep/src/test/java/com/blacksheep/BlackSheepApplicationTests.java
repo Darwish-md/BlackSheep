7bac720e99e9dfd1f7a68a984516fc1ec5203bdb
hamzariazchaudhry98 <hamzariazchaudhry98@gmail.com>
2021-04-30 00:49:01 +0200

    restyling frontend

--- BlackSheep/src/test/java/com/blacksheep/BlackSheepApplicationTests.java
+++ BlackSheep/src/test/java/com/blacksheep/BlackSheepApplicationTests.java
@@ -1,9 +1,8 @@
 package com.blacksheep;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class BlackSheepApplicationTests {
 
 	@Test
